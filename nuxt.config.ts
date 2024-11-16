58889ce3eb82a19433ecbffa32c502c69c2a5692
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2024-11-16 20:34:58 +0000

    nuxt config updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'en',
+      },
+      titleTemplate: '%s - Nuxt Components Layer',
+      meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+    },
+  },
   components: [
     {
       path: './components',
