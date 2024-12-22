e4e04baa9c3cf35427d5086f8b7760ce8481af50
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2024-12-22 21:46:28 +0000

    Tab updates

--- components/presentation/tabs/TabsCore.vue
+++ components/presentation/tabs/TabsCore.vue
@@ -18,9 +18,7 @@
     </ul>
     <div class="tab-content-wrapper">
       <div v-for="(item, key) in navItems" :key="key" class="tab-content" :aria-labelledby="`tab-${key}-trigger`" :id="`tab-${key}-content`" role="region" aria-hidden="true" ref="tabsContentRefs">
-        <div>
-          <slot :name="`tab-${key}-content`"></slot>
-        </div>
+        <slot :name="`tab-${key}-content`"></slot>
       </div>
     </div>
   </div>
@@ -69,7 +67,7 @@ onMounted(() => {
 </script>
 
 <style lang="css">
-.tabs-list {
+.tabs {
   /*
   * CSS var within /assets/styles/components/tabs.css
   */
@@ -79,121 +77,145 @@ onMounted(() => {
   --_tabs-active-indicator: var(--tabs-active-indicator, light-dark(var(--gray-12), var(--gray-0)));
   --_tabs-hovered-bg: var(--tabs-hovered-bg, light-dark(var(--gray-7), var(--gray-3)));
   --_tabs-hovered-text: var(--tabs-hovered-text, light-dark(var(--gray-0), var(--gray-12)));
-  --_tabs-border-bottom: var(--tabs-border-bottom, light-dark(var(--gray-12), var(--gray-0)));
+  --_tabs-border-bottom: var(--tabs-border-bottom, 1px solid var(--gray-6));
+  --_tabs-content-border: var(--tabs-content-border, 1px solid var(--gray-6));
+  --_tabs-content-outline: var(--tabs-content-outline, 1px solid var(--gray-6));
+  --_tabs-content-bg: var(--tabs-content-bg, light-dark(var(--gray-12), var(--gray-0)));
+  --_tabs-content-start-start-radius: var(--tabs-content-start-start-radius, 0);
+  --_tabs-content-start-end-radius: var(--tabs-content-start-end-radius, 0);
+  --_tabs-content-end-start-radius: var(--tabs-content-end-start-radius, 0);
+  --_tabs-content-end-end-radius: var(--tabs-content-end-end-radius, 0);
 
-  position: relative;
-  display: flex;
-  width: fit-content;
-  z-index: 1;
-
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-
-  .nav__hovered {
-    content: '';
-    position: absolute;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    top: 0;
-    scale: var(--_width-hovered, 0.125) 1;
-    translate: var(--_left-hovered, 0) 0;
-    transform-origin: left;
-    transition: scale var(--_transition-duration), translate var(--_transition-duration);
-    z-index: 1;
-  }
-
-  .nav__active {
-    content: '';
-    position: absolute;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    top: 0;
-    scale: var(--_width-active, 0.125) 1;
-    translate: var(--_left-active, 0) 0;
-    transform-origin: left;
-    transition: scale var(--_transition-duration), translate var(--_transition-duration);
-    z-index: 2;
-  }
-
-  .nav__active-indicator {
-    content: '';
-    position: absolute;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    scale: var(--_width-active, 0.125) 1;
-    translate: var(--_left-active, 0) 0;
-    transform-origin: left;
-    transition: scale var(--_transition-duration), translate var(--_transition-duration);
-    z-index: 3;
-  }
-
-  .tabs-list-item {
-    opacity: 0.7;
+  .tabs-list {
     position: relative;
-    transition: color 0.2s;
-    z-index: 4;
+    display: flex;
+    width: fit-content;
+    z-index: 1;
 
-    &:hover {
-      opacity: 1;
-    }
-
-    &[aria-selected='true'] {
-      opacity: 1;
-    }
-  }
-  /*
-* User configurable variables
-*/
-  border-bottom: 1px solid var(--_tabs-border-bottom);
-  margin-block: 3rem;
-
-  .nav__hovered {
-    background: var(--_tabs-hovered-bg);
-    color: var(--_tabs-hovered-text);
-  }
-
-  .nav__active {
-    background: var(--_tabs-active-bg);
-    color: var(--_tabs-active-text);
-  }
-
-  .nav__active-indicator {
-    background: var(--_tabs-active-indicator);
-    height: 4px;
-  }
-
-  .tabs-list-item {
-    background: transparent;
-    border: 0;
-    color: var(--_tabs-default-text);
-    cursor: pointer;
-    font: inherit;
-    text-transform: uppercase;
-    font-weight: 500;
+    list-style-type: none;
     margin: 0;
-    padding: 1em 2em;
+    padding: 0;
 
-    &:hover {
+    .nav__hovered {
+      content: '';
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      top: 0;
+      scale: var(--_width-hovered, 0.125) 1;
+      translate: var(--_left-hovered, 0) 0;
+      transform-origin: left;
+      transition: scale var(--_transition-duration), translate var(--_transition-duration);
+      z-index: 1;
+    }
+
+    .nav__active {
+      content: '';
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      top: 0;
+      scale: var(--_width-active, 0.125) 1;
+      translate: var(--_left-active, 0) 0;
+      transform-origin: left;
+      transition: scale var(--_transition-duration), translate var(--_transition-duration);
+      z-index: 2;
+    }
+
+    .nav__active-indicator {
+      content: '';
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      scale: var(--_width-active, 0.125) 1;
+      translate: var(--_left-active, 0) 0;
+      transform-origin: left;
+      transition: scale var(--_transition-duration), translate var(--_transition-duration);
+      z-index: 3;
+    }
+
+    .tabs-list-item {
+      opacity: 0.7;
+      position: relative;
+      transition: color 0.2s;
+      z-index: 4;
+
+      &:hover {
+        opacity: 1;
+      }
+
+      &[aria-selected='true'] {
+        opacity: 1;
+      }
+    }
+
+    border-bottom: var(--_tabs-border-bottom);
+
+    .nav__hovered {
+      background: var(--_tabs-hovered-bg);
       color: var(--_tabs-hovered-text);
     }
 
-    &[aria-selected='true'] {
+    .nav__active {
+      background: var(--_tabs-active-bg);
       color: var(--_tabs-active-text);
     }
+
+    .nav__active-indicator {
+      background: var(--_tabs-active-indicator);
+      height: 4px;
+    }
+
+    .tabs-list-item {
+      background: transparent;
+      border: 0;
+      color: var(--_tabs-default-text);
+      cursor: pointer;
+      font: inherit;
+      text-transform: uppercase;
+      font-weight: 500;
+      margin: 0;
+      padding: 1em 2em;
+
+      &:hover {
+        color: var(--_tabs-hovered-text);
+      }
+
+      &[aria-selected='true'] {
+        color: var(--_tabs-active-text);
+      }
+    }
   }
-}
 
-.tab-content-wrapper {
-  display: grid;
-  grid-template-areas: 'element-stack';
+  .tab-content-wrapper {
+    display: grid;
+    grid-template-areas: 'element-stack';
 
-  .tab-content {
-    grid-area: element-stack;
-    display: none;
+    .tab-content {
+      grid-area: element-stack;
+      display: none;
+    }
+  }
+
+  /*
+  * User configurable variables
+  */
+
+  .tab-content-wrapper {
+    background: var(--_tabs-content-bg);
+    border: var(--_tabs-content-border);
+    border-start-start-radius: var(--_tabs-content-start-start-radius);
+    border-start-end-radius: var(--_tabs-content-start-end-radius);
+    border-end-start-radius: var(--_tabs-content-end-start-radius);
+    border-end-end-radius: var(--_tabs-content-end-end-radius);
+
+    outline: var(--_tabs-content-outline);
+
+    /* .tab-content {
+    } */
   }
 }
 </style>
