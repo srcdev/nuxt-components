070488b62dc1b873fb85ed11375865dd00ae0cb5
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2025-04-20 16:58:22 +0100

    Linting tweak

--- .playground/eslint.config.mjs
+++ .playground/eslint.config.mjs
@@ -1,6 +1,24 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from './.nuxt/eslint.config.mjs';
 
-export default withNuxt(
+export default withNuxt({
   // Your custom configs here
-)
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'vue/no-multiple-template-root': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'any',
+          normal: 'any',
+          component: 'any',
+        },
+        svg: 'always',
+        math: 'always',
+      },
+    ],
+    'vue/attribute-hyphenation': ['error', 'always'], // Enforce hyphenated attribute names
+  },
+});
