d81993404fb7ea3950669cf9bac0026eb9dcad7d
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2025-08-25 16:07:52 +0100

    Refactor CSS component files - move to within playground as doesn't need to be within npm package - prevent duplication

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ["modern-normalize", "./app/assets/styles/main.css"],
+  // css: ["modern-normalize", "./app/assets/styles/main.css"],
   modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxt/image"],
   app: {
     head: {
