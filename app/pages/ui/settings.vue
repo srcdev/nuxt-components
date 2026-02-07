8e68c4b3d64aa4dd7bcd9b88124ccb9781f8ad18
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2026-02-07 12:55:00 +0000

    Settings page - Add test buttons for light-dark mode ios18 testing

--- app/pages/ui/settings.vue
+++ app/pages/ui/settings.vue
@@ -8,6 +8,14 @@
 
           <DisplayThemeSwitch />
         </LayoutRow>
+
+        <LayoutRow tag="div" variant="popout" :style-class-passthrough="['mbe-20']">
+          <h1 class="page-heading-1 mbe-18">Test form element</h1>
+
+          <div class="mbe-12">
+            <button type="button" class="test-button class-theme">This is a button</button>
+          </div>
+        </LayoutRow>
       </template>
     </NuxtLayout>
   </div>
@@ -27,4 +35,15 @@ useHead({
 });
 </script>
 
-<style lang="css"></style>
+<style lang="css">
+.settings-page {
+  .test-button {
+    all: unset;
+    padding: 0.6rem 1.2rem;
+
+    &.class-theme {
+      border: light-dark(black, white) solid 1px;
+    }
+  }
+}
+</style>
