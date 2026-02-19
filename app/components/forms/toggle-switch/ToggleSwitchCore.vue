135d7f6ca115e8e97bc43ac0601ab411ac4b99b1
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2026-02-19 22:37:51 +0000

    Input Toggle style updates

--- app/components/forms/toggle-switch/ToggleSwitchCore.vue
+++ app/components/forms/toggle-switch/ToggleSwitchCore.vue
@@ -13,7 +13,7 @@
         :required
         :checked="isChecked"
       />
-      <div class="symbol-wrapper" :class="{ round }">
+      <div class="symbol-wrapper">
         <div class="symbol" :class="symbolClasses">
           <div class="symbol-icon icon-on" :class="{ active: isChecked }">
             <slot name="iconOn">
@@ -44,7 +44,6 @@ interface Props {
   falseValue?: string | number | boolean;
   styleClassPassthrough?: string | string[];
   theme?: FormUiTheme;
-  round?: boolean;
   size?: FormSize;
   ariaDescribedby?: string;
 }
@@ -56,7 +55,6 @@ const props = withDefaults(defineProps<Props>(), {
   falseValue: false,
   styleClassPassthrough: () => [],
   theme: "default",
-  round: true,
   size: "default",
   ariaDescribedby: "",
 });
@@ -80,12 +78,10 @@ const isChecked = computed(() => {
 
 // Optimized class computeds to reduce template reactivity
 const wrapperClasses = computed(() => ({
-  round: props.round,
   "use-default-icons": useDefaultIcons.value,
 }));
 
 const symbolClasses = computed(() => ({
-  round: props.round,
   checked: isChecked.value,
 }));
 
@@ -101,6 +97,17 @@ const toggleSwitchValue = () => {
   }
 
   .toggle-switch-wrapper {
+    --_icon-on-opacity: 0;
+    --_icon-off-opacity: 1;
+    --_icon-font-size: 2.4rem;
+    --_symbol-background-color: var(--blue-12);
+    --_symbol-margin-inline-start: 0;
+    --_symbol-checked-offset: calc(var(--input-toggle-symbol-size) * 0.75);
+
+    /* New vars */
+    --_switch-padding: 0.2rem;
+    --_toggle-symbol-border-width: 0.1rem;
+
     display: flex;
     flex-direction: column;
 
@@ -111,35 +118,81 @@ const toggleSwitchValue = () => {
     }
 
     .symbol-wrapper {
+      /* Geometry */
       display: inline-flex;
       align-items: center;
       justify-content: start;
       position: relative;
       transition: background-color var(--theme-form-transition-duration);
 
+      /* UI */
+      background-color: white;
+      border: var(--form-element-border-width) solid var(--theme-input-border);
+      outline: var(--form-element-outline-width) solid var(--theme-input-outline);
+      border-radius: 100vw;
+      /* width: calc(var(--input-toggle-symbol-size) * 2); */
+      width: 72px;
+      /* height: calc(
+        var(--input-toggle-symbol-size) + calc(var(--_toggle-symbol-border-width) * 5) + calc(var(--_switch-padding) * 2)
+      ); */
+      padding: calc(var(--input-toggle-wrapper-padding) + var(--input-toggle-symbol-outline-width));
+
       &:hover {
+        background-color: var(--theme-toggle-wrapper-surface-color-hover);
         cursor: pointer;
       }
 
+      /* &.active {
+        justify-content: end;
+      } */
+
       .symbol {
+        /* Geometry */
+
         display: inline-grid;
         grid-template-areas: "icon";
         place-content: center;
-        position: absolute;
+        /* position: absolute; */
         overflow: hidden;
-        left: var(--_switch-padding);
+        translate: 0 0;
 
         aspect-ratio: 1/1;
 
+        /* UI */
+        /* padding: calc(calc(var(--input-toggle-symbol-size) - var(--_icon-font-size)) / 2); */
+        border: var(--input-toggle-symbol-border-width) solid var(--theme-checkbox-symbol-color);
+        outline: var(--input-toggle-symbol-outline-width) solid var(--theme-input-outline, red);
+        border-radius: 100vw;
+        background-color: lightskyblue;
+        transition: translate 0.4s ease;
+
         &.checked {
-          left: calc(100% - var(--_symbol-size) - var(--_switch-padding) - (2 * var(--_toggle-symbol-border-width)));
+          /* left: calc(
+            100% - var(--input-toggle-symbol-size) - var(--_switch-padding) - (2 * var(--_toggle-symbol-border-width))
+          ); */
+          /* translate: calc(var(--input-toggle-symbol-size) / 2) 0; */
+          /* translate: 1.6rem 0; */
+          translate: calc(
+              var(--input-toggle-symbol-size) + var(--input-toggle-symbol-border-width) +
+                var(--input-toggle-symbol-outline-width)
+            )
+            0;
         }
 
         .symbol-icon {
+          /* Geometry */
+
           display: grid;
           grid-area: icon;
           place-content: center;
           opacity: 0;
+
+          /* UI */
+          color: var(--theme-toggle-symbol-icon-color);
+          width: var(--input-toggle-symbol-size);
+          height: var(--input-toggle-symbol-size);
+          margin: var(--input-toggle-symbol-margin);
+          transition: opacity var(--theme-form-transition-duration);
 
           &.active {
             opacity: 1;
