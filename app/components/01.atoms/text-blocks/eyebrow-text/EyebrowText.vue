7778971524aab799adf3e521bfe2da69eaa44c6d
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2026-02-27 21:18:08 +0000

    EyebrowText added

---
+++ app/components/01.atoms/text-blocks/eyebrow-text/EyebrowText.vue
@@ -0,0 +1,40 @@
+<template>
+  <component :is="tag" class="eyebrow-text" :class="[elementClasses]">
+    {{ textContent }}
+  </component>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  tag?: "p" | "div" | "span";
+  fontSize?: "large" | "medium" | "small";
+  textContent: string;
+  styleClassPassthrough?: string | string[];
+}
+const props = withDefaults(defineProps<Props>(), {
+  tag: "div",
+  fontSize: "medium",
+  styleClassPassthrough: () => [],
+});
+
+const { elementClasses } = useStyleClassPassthrough(props.styleClassPassthrough);
+</script>
+
+<style lang="css">
+.eyebrow-text {
+  text-transform: uppercase;
+  color: var(--colour-text-eyebrow);
+
+  &.large {
+    font-size: 4.5rem;
+  }
+
+  &.medium {
+    font-size: 3.5rem;
+  }
+
+  &.small {
+    font-size: 2.5rem;
+  }
+}
+</style>
