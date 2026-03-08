eb151a0fdcd845c4217aacee91cfc87818c86f9a
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2026-03-08 17:35:18 +0000

    TextBlock - component added

---
+++ app/components/01.atoms/text-block/TextBlock.vue
@@ -0,0 +1,27 @@
+<template>
+  <component :is="tag" class="text-block" :class="[elementClasses]">
+    <slot name="default"></slot>
+  </component>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  tag?: "div" | "section" | "article" | "main";
+  styleClassPassthrough?: string | string[];
+}
+const props = withDefaults(defineProps<Props>(), {
+  tag: "div",
+  styleClassPassthrough: () => [],
+});
+
+const { elementClasses } = useStyleClassPassthrough(props.styleClassPassthrough);
+</script>
+
+<style lang="css">
+@layer components {
+  .text-block {
+    padding-block-start: var(--fluid-40-80);
+    padding-block-end: var(--fluid-40-80);
+  }
+}
+</style>
