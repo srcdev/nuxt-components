09c33da53deef052bf4a0a6cb520a2917e2eead4
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2026-03-21 22:14:30 +0000

    InputError - Fix error display classes - remove outline

--- app/components/forms/form-errors/InputError.vue
+++ app/components/forms/form-errors/InputError.vue
@@ -55,154 +55,155 @@ const { elementClasses } = useStyleClassPassthrough(props.styleClassPassthrough)
 
 <style lang="css">
 @layer components {
-.input-error-message {
-  grid-row: 2;
-  grid-column: 1;
-  display: grid;
-  grid-template-rows: 0fr;
+  .input-error-message {
+    grid-row: 2;
+    grid-column: 1;
+    display: grid;
+    grid-template-rows: 0fr;
 
-  color: var(--input-error-color);
-  background-color: var(--theme-error-surface);
-  opacity: 0;
+    color: var(--input-error-color);
+    background-color: var(--theme-error-surface);
+    opacity: 0;
 
-  transition:
-    grid-template-rows var(--theme-form-transition-duration) linear,
-    opacity var(--theme-form-transition-duration) linear,
-    margin-block-start var(--theme-form-transition-duration) linear;
+    transition:
+      grid-template-rows var(--theme-form-transition-duration) linear,
+      opacity var(--theme-form-transition-duration) linear,
+      margin-block-start var(--theme-form-transition-duration) linear;
 
-  transition-behavior: allow-discrete;
+    transition-behavior: allow-discrete;
 
-  border-radius: 0;
-  border: var(--form-element-border-width) solid transparent;
-  outline: var(--form-element-outline-width) solid transparent;
+    border-radius: 0;
+    border: var(--form-element-border-width) solid transparent;
+    outline: var(--form-element-outline-width) solid transparent;
 
-  background-clip: padding-box;
+    background-clip: padding-box;
 
-  translate: 0 calc(-1 * calc(var(--form-element-border-width) + var(--form-input-border-radius)));
+    translate: 0 calc(-1 * calc(var(--form-element-border-width) + var(--form-input-border-radius)));
 
-  margin-block-start: var(--input-error-margin-block-start);
-
-  &.underlined {
-    outline-color: transparent;
-  }
-
-  &.detached {
-    margin-block-start: 0rem;
-
-    border-top: var(--form-element-border-width) solid var(--theme-error-surface);
-    border-right: var(--form-element-border-width) solid var(--theme-error-surface);
-    border-bottom: var(--form-element-border-width) solid var(--red-08);
-    border-left: var(--form-element-border-width) solid var(--theme-error-surface);
-
-    border-radius: var(--form-input-border-radius);
+    margin-block-start: var(--input-error-margin-block-start);
 
     &.underlined {
-      border-radius: 0;
-    }
-  }
-
-  &.show {
-    grid-template-rows: 1fr;
-    display: grid;
-    opacity: 1;
-
-    border: var(--form-element-border-width) solid var(--theme-error-border);
-    outline: var(--form-element-outline-width) solid var(--theme-error-outline);
-
-    &:not(.underlined) {
-      border-bottom-left-radius: var(--form-input-border-radius);
-      border-bottom-right-radius: var(--form-input-border-radius);
+      outline-color: transparent;
     }
 
     &.detached {
-      margin-block-start: 2rem;
+      margin-block-start: 0rem;
+
+      border-top: var(--form-element-border-width) solid var(--theme-error-surface);
+      border-right: var(--form-element-border-width) solid var(--theme-error-surface);
+      border-bottom: var(--form-element-border-width) solid var(--red-08);
+      border-left: var(--form-element-border-width) solid var(--theme-error-surface);
+
+      border-radius: var(--form-input-border-radius);
+
+      &.underlined {
+        border-radius: 0;
+      }
     }
-  }
 
-  .inner {
-    align-items: center;
-    overflow: hidden;
+    &.show {
+      grid-template-rows: 1fr;
+      display: grid;
+      opacity: 1;
 
-    .inner-content {
-      display: flex;
+      border: var(--form-element-border-width) solid var(--theme-error-border);
+      outline: var(--form-element-outline-width) solid var(--theme-error-outline);
+
+      &:not(.underlined) {
+        border-bottom-left-radius: var(--form-input-border-radius);
+        border-bottom-right-radius: var(--form-input-border-radius);
+      }
+
+      &.detached {
+        margin-block-start: 2rem;
+      }
+    }
+
+    .inner {
       align-items: center;
+      overflow: hidden;
 
-      .inner-icon {
-        display: inline-block;
-        padding-left: 1.2rem;
+      .inner-content {
+        display: flex;
+        align-items: center;
 
-        .icon {
-          color: white;
-          transform: translateY(3px);
-        }
-      }
+        .inner-icon {
+          display: inline-block;
+          padding-left: 1.2rem;
 
-      .message {
-        display: inline-block;
-        flex-grow: 1;
-        font-family: var(--font-family);
-        font-size: 1.6rem;
-        font-weight: 500;
-        padding-block: 1rem 1rem;
-        padding-inline: 1.2rem;
-
-        .message-single {
-          color: var(--input-error-color);
-        }
-
-        .message-list {
-          list-style-type: none;
-          padding-inline-start: 0;
-          margin-block-start: 0;
-          margin-block-end: 0;
-
-          .message-list-item {
+          .icon {
             color: white;
+            transform: translateY(3px);
+          }
+        }
+
+        .message {
+          display: inline-block;
+          flex-grow: 1;
+          font-family: var(--font-family);
+          font-size: 1.6rem;
+          font-weight: 500;
+          padding-block: 1rem 1rem;
+          padding-inline: 1.2rem;
+
+          .message-single {
+            color: var(--input-error-color);
           }
 
-          .message-list-item + .message-list-item {
-            margin-block-start: 0.6rem;
+          .message-list {
+            list-style-type: none;
+            padding-inline-start: 0;
+            margin-block-start: 0;
+            margin-block-end: 0;
+
+            .message-list-item {
+              color: white;
+            }
+
+            .message-list-item + .message-list-item {
+              margin-block-start: 0.6rem;
+            }
+          }
+        }
+      }
+    }
+
+    /* Modifiers for input variants: */
+    &:is(.detached) {
+      &.normal {
+        .inner {
+          .inner-content {
+            .message {
+              padding-block: 1.2rem 1rem;
+            }
+          }
+        }
+      }
+    }
+
+    &:not(.detached) {
+      &.normal {
+        .inner {
+          padding-block-start: var(--form-input-border-radius);
+
+          .inner-content {
+            .message {
+              padding-block: 1.2rem 1rem;
+            }
+          }
+        }
+      }
+      &.underlined {
+        outline-color: transparent;
+        .inner {
+          .inner-content {
+            .message {
+              padding-block: 1rem 1rem;
+            }
           }
         }
       }
     }
   }
-
-  /* Modifiers for input variants: */
-  &:is(.detached) {
-    &.normal {
-      .inner {
-        .inner-content {
-          .message {
-            padding-block: 1.2rem 1rem;
-          }
-        }
-      }
-    }
-  }
-
-  &:not(.detached) {
-    &.normal {
-      .inner {
-        padding-block-start: var(--form-input-border-radius);
-
-        .inner-content {
-          .message {
-            padding-block: 1.2rem 1rem;
-          }
-        }
-      }
-    }
-    &.underlined {
-      .inner {
-        .inner-content {
-          .message {
-            padding-block: 1rem 1rem;
-          }
-        }
-      }
-    }
-  }
-}
 }
 </style>
